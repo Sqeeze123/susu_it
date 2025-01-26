3cfe2c7f30d6863d5a73d10c54b5218cf6d4e027
nik <NIKITA@example.com>
2025-01-26 23:39:16 +0500

    Update: changed output message in feature branch

--- distance.py
+++ distance.py
@@ -1,4 +1,4 @@
-import math
+import mathimport math
 
 x0 = float(input("Введите x0: "))
 y0 = float(input("Введите y0: "))
@@ -6,4 +6,4 @@ x1 = float(input("Введите x1: "))
 y1 = float(input("Введите y1: "))
 
 distance = math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
-print("Расстояние между двумя точками:", distance)
+print(f"Расстояние между двумя точками на плоскости ({x0}, {y0}) и ({x1}, {y1}): {distance}"))
