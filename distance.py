aac045045fd4f10875fe2a4927d970c6fa248cc8
nik <NIKITA@example.com>
2025-01-26 21:39:27 +0500

    Changed variable name in develop

--- distance.py
+++ distance.py
@@ -1,9 +1,10 @@
 import math
 
-x0 = float(input("Введите x0: "))
+first_x = float(input("Введите x0: "))
 y0 = float(input("Введите y0: "))
 x1 = float(input("Введите x1: "))
 y1 = float(input("Введите y1: "))
 
-distance = math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
-print(f"Расстояние между двумя точками ({x0}, {y0}) и ({x1}, {y1}): {distance}")
+
+distance = math.sqrt((x1 - first_x)**2 + (y1 - y0)**2)
+print(f"Расстояние ({first_x}, {y0}) и ({x1}, {y1}): {distance}")
